dcf3fb388b5d790b8660cb0c3cb830903f175992
DanielHreggvidsson <danielhreggvids@gmail.com>
2023-11-02 19:46:48 +0000

    klára javasript og breyta json svo lib skrár séu í build

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { createCartLine, showCartContent } from './lib/ui.js';
+import { createCartLine, updateCartTotal, showCartContent} from './lib/ui.js';
+import { formatPrice } from './lib/helpers.js';
 
 const products = [
   {
@@ -21,25 +22,52 @@ const products = [
     price: 20_000,
   },
 ];
+function findCartLine(product) {
+  const cartTableBodyElement = document.querySelector('.cart table tbody');
+  if (!cartTableBodyElement) {
+    return null;
+  }
+
+  const existingCartLines = cartTableBodyElement.querySelectorAll(`tr[data-product-id="${product.id}"]`);
+  if (existingCartLines.length > 0) {
+    return existingCartLines[0]; 
+  }
+
+  return null;
+}
 
 /** Bæta vöru í körfu */
 function addProductToCart(product, quantity) {
   // Hér þarf að finna `<tbody>` í töflu og setja `cartLine` inn í það
-  const cart = document.querySelector('.cart-content');
+  const cartTableBodyElement = document.querySelector('.cart table tbody');
+  
 
-  if (!cart) {
-    console.warn('fann ekki .cart');
+  if (!cartTableBodyElement) {
+    console.warn('fann ekki .cart table');
     return;
+  }
+  const existingCartLine = findCartLine(product);
+  
+  if(existingCartLine){
+    const existingQuantityElement = existingCartLine.querySelector('.quantity');
+    const existingQuantity = parseInt(existingQuantityElement.textContent, 10);
+    const newQuantity = existingQuantity + quantity;
+    existingQuantityElement.textContent = newQuantity.toString();
+
+    const newTotal = newQuantity * product.price;
+    existingCartLine.querySelector('.total').textContent = formatPrice(newTotal);
   }
   
   // TODO hér þarf að athuga hvort lína fyrir vöruna sé þegar til
+  else{
   const cartLine = createCartLine(product, quantity);
-  cart.appendChild(cartLine);
-
+  cartTableBodyElement.appendChild(cartLine);
+  }
   // Sýna efni körfu
   showCartContent(true);
 
-  // TODO sýna/uppfæra samtölu körfu
+  updateCartTotal();
+
 }
 
 function submitHandler(event) {
@@ -47,7 +75,7 @@ function submitHandler(event) {
   event.preventDefault();
   
   // Finnum næsta element sem er `<tr>`
-  const parent = event.target.closest('tr')
+  const parent = event.target.closest('tr');
 
   // Það er með attribute sem tiltekur auðkenni vöru, t.d. `data-product-id="1"`
   const productId = Number.parseInt(parent.dataset.productId);
@@ -57,7 +85,23 @@ function submitHandler(event) {
 
   // TODO hér þarf að finna fjölda sem á að bæta við körfu með því að athuga
   // á input
-  const quantity = 1;
+
+  const quantityInput = parent.querySelector('input[type="number"]');
+
+  if (!quantityInput) {
+    console.warn('Gildi fannst ekki');
+    return;
+  }
+
+
+  const quantity = parseInt(quantityInput.value, 10);
+
+ 
+  if (isNaN(quantity) || quantity <= 0) {
+    console.warn('Ólöglegt gildi');
+    return;
+  }
+  
 
   // Bætum vöru í körfu (hér væri gott að bæta við athugun á því að varan sé til)
   addProductToCart(product, quantity);
@@ -71,5 +115,21 @@ for (const form of Array.from(addToCartForms)) {
   // Bæta submit event listener við hvert
   form.addEventListener('submit', submitHandler);
 }
+function showReceipt(event) {
+  event.preventDefault();
+  const productsclass=document.querySelector('.products');
+  const cart=document.querySelector('.cart');
+  const receipt=document.querySelector('.receipt');
+  if(productsclass && cart && receipt){
+    productsclass.classList.add('hidden');
+    cart.classList.add('hidden');
+    receipt.classList.remove('hidden');
+  } 
+ 
+}
 
-// TODO bæta við event handler á form sem submittar pöntun
+const receiptLink = document.querySelector('.takki');
+if (receiptLink){
+receiptLink.addEventListener('submit', showReceipt);
+}
+
